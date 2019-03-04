36e9749f24836e85457ea2878d6db0a023706532
cyij <cyij2006@gmail.com>
2019-03-05 00:49:55 +0800

    typo

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
 import { Collection, Db, MongoClient, MongoClientOptions } from 'mongodb';
 
-export default interface MysqlDao {
+export default interface MongoDao {
   connect (): Promise<MongoClient>;
   getClient (): MongoClient;
   disconnect (): Promise<void>;
